b6924880ca7a17df0c0ffde4e9744a082e076d58
itros <itros97@gmail.com>
2025-12-30 17:43:10 +0100

    added default seed and data

---
+++ composeApp/src/commonMain/resources/seed/syllables_default.sql
@@ -0,0 +1,5 @@
+INSERT INTO syllables(type, category, value, weight) VALUES
+    ('prefix', 'name', 'ka', 2),
+    ('prefix', 'name', 'el', 1),
+    ('core',   'name', 'dor', 1),
+    ('suffix', 'name', 'eth', 3);
