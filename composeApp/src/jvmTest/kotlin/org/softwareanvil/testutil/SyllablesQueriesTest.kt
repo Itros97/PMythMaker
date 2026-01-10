a9b756eae8eb790a08107048d3402ccc91882204
itros97 <itros97@gmail.com>
2026-01-11 00:25:57 +0100

    Refactored repository structure and added Syllable model and repository interface

--- composeApp/src/jvmTest/kotlin/org/softwareanvil/testutil/SyllablesQueriesTest.kt
+++ composeApp/src/jvmTest/kotlin/org/softwareanvil/testutil/SyllablesQueriesTest.kt
@@ -10,59 +10,6 @@ class SyllablesQueriesTest {
     fun insert_and_select_syllables_work() {
         val db = createTestDatabase()
 
-        // Insertar datos
-        db.syllablesQueries.insert(
-            type = "prefix",
-            category = "name",
-            syllable = "ka",
-            weight = 2
-        )
 
-        db.syllablesQueries.insert(
-            type = "core",
-            category = "name",
-            syllable = "dor",
-            weight = 1
-        )
-
-        // Leer datos
-        val prefixes = db.syllablesQueries
-            .selectByType("prefix")
-            .executeAsList()
-
-        // Verificaciones
-        assertEquals(1, prefixes.size)
-        assertEquals("ka", prefixes.first().syllable)
-        assertEquals(2, prefixes.first().weight)
     }
-
-    @Test
-    fun delete_single_syllable_by_id() {
-        val db = createTestDatabase()
-
-        // Insertamos dos sílabas
-        db.syllablesQueries.insert("prefix", "name", "ka", 1)
-        db.syllablesQueries.insert("prefix", "name", "el", 1)
-
-        // Leemos todas
-        val syllables = db.syllablesQueries
-            .selectByType("prefix")
-            .executeAsList()
-
-        assertEquals(2, syllables.size)
-
-        val idToDelete = syllables.first().id
-
-        // Borramos solo una
-        db.syllablesQueries.deleteById(idToDelete)
-
-        // Volvemos a leer
-        val remaining = db.syllablesQueries
-            .selectByType("prefix")
-            .executeAsList()
-
-        assertEquals(1, remaining.size)
-        assertEquals("el", remaining.first().syllable)
-    }
-
 }
